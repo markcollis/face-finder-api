4a03536adb818465ad0314e88669e4f66d2b174f
Mark Collis <mark@markandblanka.com>
2018-10-22 20:31:30 +0200

    Initial starting point

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('this is working');
+});
+
+app.listen(3001, () => {
+  console.log('app is running on port 3001');
+});
